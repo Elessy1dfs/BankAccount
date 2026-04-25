f4f3ce16b061ce31ad8118c20cf0b9576d1ea8de
Eleonora Sayson <saysoneleonora@gmail.com>
2026-04-25 08:15:31 +0800

    SAYSON--Make BankAccounts class abstract and implement Transactional
    Refactor BankAccounts class to be abstract and implement Transactional interface.

--- BankAccount/BankAccounts.java
+++ BankAccount/BankAccounts.java
@@ -1,6 +1,7 @@
-BankAccounts{
+public abstract class BankAccounts implements Transactional {
+    private String name;
+    private String pin;
+    protected double balance;
 
- System.out.println("hi guys mag start na niya tag code ani");
-
-
+ 
 }
