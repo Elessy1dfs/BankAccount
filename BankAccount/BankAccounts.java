2ef904565ca3dc4556edfc79f2a3d80f8b39ef04
Eleonora Sayson <saysoneleonora@gmail.com>
2026-04-25 08:17:29 +0800

    SAYSON--Added constructor to BankAccount class

--- BankAccount/BankAccounts.java
+++ BankAccount/BankAccounts.java
@@ -3,5 +3,11 @@ public abstract class BankAccounts implements Transactional {
     private String pin;
     protected double balance;
 
+    public BankAccount(String name, double balance, String pin) {
+        this.name = name;
+        this.balance = balance;
+        this.pin = pin;
+    }
+
  
 }
