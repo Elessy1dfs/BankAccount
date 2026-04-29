32a4085d380f0b38b65ce015d1d851177a3beba5
Maraiah Carmel Bellita <schoolpurposesmel@gmail.com>
2026-04-29 21:40:38 +0800

    feat(bank): implement addAccount and findAccount logic

--- src/bank/BankSystem.java
+++ src/bank/BankSystem.java
@@ -6,4 +6,24 @@ public class BankSystem {
     public BankSystem(){
         this.accounts = new ArrayList<BankAccount>();
     }
+    
+    //function para mo add og account
+    public void addAccount(BankAccount account){
+        this.accounts.add(account);
+    }
+
+    //function para mo pangita sa account
+    public BankAccount findAccount(String name){
+        for(int i = 0; i < this.accounts.size(); i++){
+            BankAccount currentAccount = this.accounts.get(i);
+
+            if(currentAccount.getName().equals(name)){
+                return currentAccount;
+            }
+        }
+
+        return null;
+    }
+
+
 }
