a927e2ac447b7b893ca251e42bd33f03c290a687
Maraiah Carmel Bellita <schoolpurposesmel@gmail.com>
2026-04-29 21:36:19 +0800

    feat(bank): initialize BankSystem and ArrayList structure

---
+++ src/bank/BankSystem.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class BankSystem {
+    private ArrayList<BankAccount> accounts;
+
+    public BankSystem(){
+        this.accounts = new ArrayList<BankAccount>();
+    }
+}
