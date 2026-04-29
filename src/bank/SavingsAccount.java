763e3657878ce1ca48cae0b44990a0e0346a52b5
DynamicCydric <mulajamessedric@gmail.com>
2026-04-29 14:52:27 +0800

    added SavingsAccount class

--- src/bank/SavingsAccount.java
+++ src/bank/SavingsAccount.java
@@ -1,4 +1,30 @@
-public class SavingsAccount {
+class SavingsAccount extends BankAccount {
 
-    System.out.println("Savings ni");
+    public SavingsAccount(String name, double balance, String pin) {
+        super(name, balance, pin);
+    }
+
+    @Override
+    public void withdraw(double amount, String inputPin) {
+
+        if (!validatePin(inputPin)) {
+            System.out.println("Incorrect PIN!");
+            return;
+        }
+
+        if (amount > balance) {
+            System.out.println("Insufficient funds!");
+        } else if (balance - amount < 100) {
+            System.out.println("Must maintain minimum balance of $100!");
+        } else {
+            balance -= amount;
+            history.addRecord("Withdrew: $" + amount);
+            System.out.println("Withdrawal successful!");
+        }
+    }
+
+    @Override
+    public void displayAccountType() {
+        System.out.println("Account Type: SAVINGS");
+    }
 }
