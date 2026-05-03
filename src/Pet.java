952a64a5bf263c9c3db6def98d955b0bdcfcc307
Seanton2 <seananthony.sanchez@cit.edu>
2026-05-03 11:46:04 +0800

    feat: add Pet constructor with sprite loading and initial position setup

--- src/Pet.java
+++ src/Pet.java
@@ -1,13 +1,32 @@
 import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
 
 public abstract class Pet {
     public String name;
     public int hp, maxHp, aura, rizz;
     public BufferedImage spriteSheet;
     public int frameIndex = 0;
-    public int state = 0; // 0: Idle, 1: Attack, 2: Hit
+    public int state = 0;
 
-    // Position variables for movement
     public int x, y;
     public int baseX, baseY;
+
+    public Pet(String name, int hp, int aura, int rizz, String spritePath, int bx, int by) {
+        this.name = name;
+        this.hp = hp;
+        this.maxHp = hp;
+        this.aura = aura;
+        this.rizz = rizz;
+        this.baseX = bx;
+        this.baseY = by;
+        this.x = bx;
+        this.y = by;
+
+        try {
+            this.spriteSheet = ImageIO.read(new File(spritePath));
+        } catch (Exception e) {
+            System.out.println("CRITICAL: Image not found at " + spritePath);
+        }
+    }
 }
