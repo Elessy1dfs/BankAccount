d9db69c7648759182e8e7518a6ca67e3ae5d5aa3
Seanton2 <seananthony.sanchez@cit.edu>
2026-05-11 01:46:32 +0800

    feat: add sprite loading and animation state variables

--- src/Pet.java
+++ src/Pet.java
@@ -1,10 +1,18 @@
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+ 
 public abstract class Pet {
     public String name;
     public int hp, maxHp, aura, rizz;
     public int mana, maxMana = 100;
     public int x, y;
     public int faceDir = -1;
- 
+    public int frameIndex = 0;
+    public int state = 0; 
+    public BufferedImage spriteSheet;
+    
     public Pet(String name, int hp, int aura, int rizz, String spritePath, int x, int y) {
         this.name = name;
         this.hp = hp;
@@ -14,5 +22,12 @@ public abstract class Pet {
         this.rizz = rizz;
         this.x = x;
         this.y = y;
+          try {
+        this.spriteSheet = ImageIO.read(new File(spritePath));
+    } catch (Exception e) {
+        System.out.println("Resource error: " + spritePath);
     }
+    }
+        
+    
 }
